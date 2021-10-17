6b76e3c39f8b6af3ec917d05ed2b9145a92c7aed
atulkalantre <88487294+atulkalantre@users.noreply.github.com>
2021-10-17 19:17:10 +0530

    [+][1 - Test Case][Add String Numbers]

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,16 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        int finalSum = 0;
+        String[] inputArray = input.split(",");
+        if (inputArray.length > 2) {
+            throw new RuntimeException("Up to 2 numbers are allowed only.");
+        }
+        for (String number : inputArray) {
+            if (!number.trim().isEmpty()) {
+                finalSum += Integer.parseInt(number);
+            }
+        }
+        return finalSum;
     }
-
 }
