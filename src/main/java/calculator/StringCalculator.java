affad3aa8ea5716cbe2fd4fe538eb83e78f8112b
atulkalantre <88487294+atulkalantre@users.noreply.github.com>
2021-10-17 19:28:00 +0530

    [+][2 - Test Case][Handle Unknown Amount of Numbers]

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,9 +5,9 @@ class StringCalculator {
     public int add(String input) {
         int finalSum = 0;
         String[] inputArray = input.split(",");
-        if (inputArray.length > 2) {
-            throw new RuntimeException("Up to 2 numbers are allowed only.");
-        }
+//        if (inputArray.length > 2) {
+//            throw new RuntimeException("Up to 2 numbers are allowed only.");
+//        }
         for (String number : inputArray) {
             if (!number.trim().isEmpty()) {
                 finalSum += Integer.parseInt(number);
