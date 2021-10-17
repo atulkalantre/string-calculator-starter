f584f256eb911a13ecd04254c62b1e770f0fbda5
atulkalantre <88487294+atulkalantre@users.noreply.github.com>
2021-10-17 23:47:57 +0530

    [+][3rd - Test Case][Allow to handle new lines]

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,13 +4,10 @@ class StringCalculator {
 
     public int add(String input) {
         int finalSum = 0;
-        String[] inputArray = input.split(",");
-//        if (inputArray.length > 2) {
-//            throw new RuntimeException("Up to 2 numbers are allowed only.");
-//        }
+        String[] inputArray = input.split(",|n");
         for (String number : inputArray) {
             if (!number.trim().isEmpty()) {
-                finalSum += Integer.parseInt(number);
+                finalSum += Integer.parseInt(number.trim());
             }
         }
         return finalSum;
