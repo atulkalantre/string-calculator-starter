d4b5fd340c252b745393f5ea6d956aa0aeccbf48
atulkalantre <88487294+atulkalantre@users.noreply.github.com>
2021-10-18 00:36:42 +0530

    [+][5th - Test Case][Throw an exception on negative numbers]

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,26 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class StringCalculator {
 
     public int add(String input) {
-        int finalSum = 0;
-        String[] inputArray = input.split(",|n");
+    	int finalSum = 0;
+        String[] inputArray = input.split(",");
+        List negativeNumbers = new ArrayList();
         for (String number : inputArray) {
             if (!number.trim().isEmpty()) {
-                finalSum += Integer.parseInt(number.trim());
+                int intNum = Integer.parseInt(number.trim());
+                if (intNum < 0) {
+                    negativeNumbers.add(intNum);
+                }
+                finalSum += intNum;
             }
         }
-        return finalSum;
+        if (negativeNumbers.size() > 0) {
+            throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+        }
+        return finalSum;     
     }
 }
